d99cd12ae441be24b39393618607e144b2568b20
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 19:05:37 -0300

    ✨ create view page to tasks and projects

---
+++ src/views/Projects.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="projects">
+    <h1 class="title">Projetos</h1>
+
+    <form action="" @submit.prevent="saveProject">
+      <label for="project-name" class="label"></label>
+      <input
+        type="text"
+        name="project-name"
+        id="project-name"
+        class="input"
+        placeholder="Nome do projeto"
+        v-model="projectName"
+      />
+      <div class="field">
+        <button class="button" type="submit">Save</button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import IProject from "@/interfaces/IProject";
+
+export default defineComponent({
+  name: "projects-page",
+  data() {
+    return {
+      projectName: "",
+      projects: [] as IProject[],
+    };
+  },
+  methods: {
+    saveProject(): void {
+      console.log(this.projectName);
+      const project: IProject = {
+        id: new Date().toISOString(),
+        name: this.projectName,
+      };
+      this.projects.push(project);
+      this.projectName = "";
+    },
+  },
+});
+</script>
+
+<style scoped>
+.projects {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/views/Tasks.vue
@@ -0,0 +1,39 @@
+<template>
+  <FormNewTask @on-end-task="addTask" />
+  <div class="list">
+    <Task v-for="(task, index) in tasks" :key="index" :task="task" />
+    <BoxVue v-if="isTasksEmpty"> Você não está muito produtivo hoje :( </BoxVue>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import FormNewTask from "@/components/FormNewTask.vue";
+import Task from "@/components/Task.vue";
+import ITask from "@/interfaces/ITask";
+import BoxVue from "@/components/Box.vue";
+
+export default defineComponent({
+  name: "tasks-page",
+  components: {
+    FormNewTask,
+    Task,
+    BoxVue,
+  },
+  data() {
+    return {
+      tasks: [] as ITask[],
+    };
+  },
+  computed: {
+    isTasksEmpty(): boolean {
+      return this.tasks.length === 0;
+    },
+  },
+  methods: {
+    addTask(task: ITask): void {
+      this.tasks.push(task);
+    },
+  },
+});
+</script>
