96a9baa91458dcd5d08602d24195846b59968d9b
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 19:06:21 -0300

    🔨 fix render page

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,7 @@
       <SideBar @clicked="changeTheme" />
     </div>
     <div class="column is-three-quarter content">
-      <FormNewTask @on-end-task="addTask" />
-      <div class="list">
-        <Task v-for="(task, index) in tasks" :key="index" :task="task" />
-        <BoxVue v-if="isTasksEmpty">
-          Você não está muito produtivo hoje :(
-        </BoxVue>
-      </div>
+      <router-view />
     </div>
   </main>
 </template>
@@ -21,34 +15,18 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SideBar from "@/components/SideBar.vue";
-import FormNewTask from "@/components/FormNewTask.vue";
-import Task from "@/components/Task.vue";
-import ITask from "@/interfaces/ITask";
-import BoxVue from "@/components/Box.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     SideBar,
-    FormNewTask,
-    Task,
-    BoxVue,
   },
   data() {
     return {
-      tasks: [] as ITask[],
       isDarkMode: false,
     };
   },
-  computed: {
-    isTasksEmpty(): boolean {
-      return this.tasks.length === 0;
-    },
-  },
   methods: {
-    addTask(task: ITask): void {
-      this.tasks.push(task);
-    },
     changeTheme(darkMode: boolean): void {
       this.isDarkMode = darkMode;
     },
