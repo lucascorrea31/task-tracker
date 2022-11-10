a6647616b064a406ca11f64c27c04790c18e661e
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 15:12:31 -0300

    ✨ new component Cronometer

---
+++ src/components/Cronometer.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <strong>{{ timeFormatted }}</strong>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "cron-timer",
+  props: {
+    time: {
+      type: Number,
+      default: 0,
+      require: true,
+    },
+  },
+  computed: {
+    timeFormatted(): string {
+      return new Date(this.time * 1000).toISOString().substring(11, 19);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
