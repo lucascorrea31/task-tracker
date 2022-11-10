719dd7a6766aa144fcce4234181924f682127f98
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 14:05:59 -0300

    ✨ add new components

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormNewTask />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from '@/components/BarraLateral.vue';
+import FormNewTask from '@/components/FormNewTask.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    FormNewTask
+  }
+});
+</script>
+
+<style>
+</style>
