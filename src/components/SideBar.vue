198a1e1d8c4d62d9cafaecb2eb6900d3e38d952e
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 19:05:53 -0300

    🔨 create menubar to access pages

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,7 +3,25 @@
     <h1>
       <img src="@/assets/logo.png" alt="Logo Task Tracker" />
     </h1>
-    <button class="button" @click="handleClick">{{ btnLabel }}</button>
+    <div class="has-text-centered">
+      <button class="button" @click="handleClick">{{ btnLabel }}</button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projects" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -34,17 +52,34 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
 header {
-  padding: 1rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
+  padding: 2rem;
 }
 @media only screen and (max-width: 768px) {
   header {
-    padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
