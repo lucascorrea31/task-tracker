f950f0ad5ab2aaefe2c80d7697dfa5a0a2474a5a
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 15:31:02 -0300

    ✨ add new component EventButton

---
+++ src/components/EventButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="button" @click="handleClick" :disabled="disabled">
+    <span class="icon">
+      <i :class="icon"></i>
+    </span>
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "event-button",
+  props: {
+    label: {
+      type: String,
+      default: "",
+      require: true,
+    },
+    icon: {
+      type: String,
+      default: "",
+      require: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      require: true,
+    },
+  },
+  methods: {
+    handleClick(): void {
+      this.$emit("clicked");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
