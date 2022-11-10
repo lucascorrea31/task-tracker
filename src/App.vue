928fb2e828c9e1ad6c012385eef5e5c5b7863a95
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 16:03:59 -0300

    🔨 add task list and function to add current task on finish

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,43 @@
       <SideBar />
     </div>
     <div class="column is-three-quarter">
-      <FormNewTask />
+      <FormNewTask @on-end-task="addTask" />
+      <div class="list">
+        <Task v-for="(task, index) in tasks" :key="index" :task="task" />
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import SideBar from '@/components/SideBar.vue';
-import FormNewTask from '@/components/FormNewTask.vue';
+import { defineComponent } from "vue";
+import SideBar from "@/components/SideBar.vue";
+import FormNewTask from "@/components/FormNewTask.vue";
+import Task from "@/components/Task.vue";
+import ITask from "@/interfaces/ITask";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     SideBar,
-    FormNewTask
-  }
+    FormNewTask,
+    Task,
+  },
+  data() {
+    return {
+      tasks: [] as ITask[],
+    };
+  },
+  methods: {
+    addTask(task: ITask): void {
+      this.tasks.push(task);
+    },
+  },
 });
 </script>
 
 <style>
+.list {
+  padding: 1.25rem;
+}
 </style>
