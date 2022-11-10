0619181b48930f0091ed90c05e5fd4f02be00887
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-10 14:22:17 -0300

    🚚 rename sideBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <SideBar />
     </div>
     <div class="column is-three-quarter">
       <FormNewTask />
@@ -11,13 +11,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import BarraLateral from '@/components/BarraLateral.vue';
+import SideBar from '@/components/SideBar.vue';
 import FormNewTask from '@/components/FormNewTask.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    BarraLateral,
+    SideBar,
     FormNewTask
   }
 });
