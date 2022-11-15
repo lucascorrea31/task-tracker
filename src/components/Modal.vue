693f179ce52e3e023fcc840a7d213954e7e7d414
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-14 23:11:58 -0300

    ✨ new modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal" :class="{ 'is-active': isActive }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <slot name="header"></slot>
+      </header>
+      <section class="modal-card-body">
+        <slot name="body"></slot>
+      </section>
+      <footer class="modal-card-foot">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "modal-action",
+  props: {
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
