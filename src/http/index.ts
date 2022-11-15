4bae988b423537f9f238fe48dedb8e2c1e96dadd
Lucas Corrêa <lucas.nhl@gmail.com>
2022-11-14 23:11:18 -0300

    🔨 add default header

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,6 +2,9 @@ import axios, { AxiosInstance } from 'axios';
 
 const httpClient: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3000/',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 export default httpClient;
